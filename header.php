b6ae9269a7a6d6077e5840098facce9c2a8c9446
john andrey <andrey_201015@hotmail.com>
2016-07-03 16:53:22 -0500

    Modificando la carga de estilos

--- header.php
+++ header.php
@@ -16,8 +16,8 @@
         <link rel="mask-icon" href="<?php bloginfo('template_url')?>/img/icono/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="theme-color" content="#ffffff">
 
-        <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/normalize.min.css">
-        <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/main.css">
+        <!-- <link rel="stylesheet" href="<?php //bloginfo('template_url')?>/css/normalize.min.css">
+        <link rel="stylesheet" href="<?php //bloginfo('template_url')?>/css/main.css"> -->
         <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
         <link href='https://fonts.googleapis.com/css?family=Passion+One|Roboto:400,700,500,900' rel='stylesheet' type='text/css'>
